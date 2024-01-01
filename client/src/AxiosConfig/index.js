bf16a8c629dc85cb8a6430ba627a31c5791be0ad
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2024-01-02 01:42:36 +0530

    Backend deployed url updated.

--- client/src/AxiosConfig/index.js
+++ client/src/AxiosConfig/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 var BackendUrl = "http://localhost:8000/api/v1"
 if (process.env.MODE === "PRODUCTION") {
-    BackendUrl = "https://d-zap.onrender.com/api/v1"
+    BackendUrl = "https://dzap-backend-server.onrender.com/"
 }
 
 const api = axios.create({ baseURL: BackendUrl })
